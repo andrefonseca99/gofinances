5ef8a3d777675f63635377c50e6d9284fc87f274
André Pereira Fonseca <andrepfonseca19@gmail.com>
2022-10-19 12:40:31 -0300

    Creating input component

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { 
+    Container,
+    Header,
+    Title,
+    Form
+} from "./styles";
+import { Input } from "../../components/Forms/Input";
+
+export function Register(){
+    return(
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+            <Form>
+                <Input
+                    placeholder="Nome"
+                />
+                <Input
+                    placeholder="Preço"
+                />
+            </Form>
+        </Container>
+    );
+}
