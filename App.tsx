7e2203a05945998f5acfff163720039667a47455
André Pereira Fonseca <andrepfonseca19@gmail.com>
2022-10-20 11:41:39 -0300

    Styling Category interface

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   )
 }
