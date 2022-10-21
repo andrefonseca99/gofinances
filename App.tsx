6fc864340c3852cc031bd2e9921003b4b439590b
André Pereira Fonseca <andrepfonseca19@gmail.com>
2022-10-21 10:04:55 -0300

    Using React Navigation (Tab Navigation)

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from "@react-navigation/native";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
-} from '@expo-google-fonts/poppins';
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme'
-import { Register } from './src/screens/Register';
+import theme from "./src/global/styles/theme";
+import { Register } from "./src/screens/Register";
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +29,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+
     </ThemeProvider>
   )
 }
