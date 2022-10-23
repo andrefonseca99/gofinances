7ffbc63494682ea9c70afb4d59070238c78c51d3
André Pereira Fonseca <andrepfonseca19@gmail.com>
2022-10-23 18:31:08 -0300

    Listing transactions in Dashboard

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -46,9 +46,6 @@ const schema = Yup.object().shape({
 export function Register(){
     const [transactionType, setTransactionType] = useState('');
     const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-
-    const dataKey = "@gofinances:transactions";
-
     const [category , setCategory] = useState({
         key: 'category',
         name: 'Categoria'
@@ -100,9 +97,9 @@ export function Register(){
         }
 
         try {
+            const dataKey = "@gofinances:transactions";
             const data = await AsyncStorage.getItem(dataKey);
             const currentData = data ? JSON.parse(data) : [];
-
             const dataFormatted = [
                 ...currentData,
                 newTransaction
